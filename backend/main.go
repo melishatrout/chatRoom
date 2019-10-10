2fe5d11c076e7ebbf177749cdeddea0e658e23ab
melisha <melishatrout@gmail.com>
2019-10-10 21:39:15 +0100

    Pushing changes

--- backend/main.go
+++ backend/main.go
@@ -6,25 +6,33 @@ import (
 	"net/http"
 )
 
-func serveWs(w http.ResponseWriter, r *http.Request) {
-
-	ws, err := websocket.Upgrade(w, r)
+func serveWs(pool *websocket.Pool, w http.ResponseWriter, r *http.Request) {
+	fmt.Println("WebSocket Endpoint Hit")
+	conn, err := websocket.Upgrade(w, r)
 	if err != nil {
 		fmt.Fprint(w, "%+V\n", err)
 	}
 
-	go websocket.Writer(ws)
-	websocket.Reader(ws)
+	client := &websocket.Client{
+		Conn: conn,
+		Pool: pool,
+	}
+	pool.Register <- client
+	client.Read()
 
 }
 
 func setUpRoutes() {
-	http.HandleFunc("/ws", serveWs)
+	pool := websocket.NewPool()
+	go pool.Start()
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		serveWs(pool, w, r)
+	})
 
 }
 
 func main() {
-	fmt.Println("Chat App")
+	fmt.Println("Distributed Chat App v0.01")
 	setUpRoutes()
 	http.ListenAndServe(":8080", nil)
 
--- backend/pkg/websocket/Client.go
+++ backend/pkg/websocket/Client.go
@@ -9,14 +9,30 @@ import (
 )
 
 type Client struct {
-	ID string
+	ID   string
 	Conn *websocket.Conn
-	Pool *sync.Pool
+	Pool *Pool
 }
 
 type Message struct {
-	Type int `json:"type"`
+	Type int    `json:"type"`
 	Body string `json:"body"`
 }
 
+func (c *Client) Read() {
+	defer func() {
+		c.Pool.Unregister <- c
+		c.Conn.Close()
+	}()
 
+	for {
+		messageType, p, err := c.Conn.ReadMessage()
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		message := Message{Type: messageType, Body: string(p)}
+		c.Pool.Broadcast <- message
+		fmt.Printf("Message Receievd: %+v\n", message)
+	}
+}
